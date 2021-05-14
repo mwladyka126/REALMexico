83abe5e463f30a9ec2f2d9466f67c1171cc49de1
mwladyka126 <71927655+mwladyka126@users.noreply.github.com>
2021-05-14 22:33:32 +0200

    add header and basic structure for footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,16 +1,121 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import clsx from 'clsx';
+import { Container, Row, Col, Button } from "reactstrap";
+
+import clsx from "clsx";
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
-import styles from './Footer.module.scss';
+import styles from "./Footer.module.scss";
+import {
+  faCalendarAlt,
+  faEnvelope,
+  faMapMarkedAlt,
+  faPhone,
+  faLongArrowAltRight,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faGooglePlusG,
+  faTwitter,
+  faPinterest,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Component = ({className, children}) => (
+const Component = ({ className, children }) => (
   <div className={clsx(className, styles.root)}>
-    <h2>Footer</h2>
+    <footer className={styles.footer}>
+      <Container className={styles.footer__top}>
+        <div className={styles.footer__top__title}>
+          <p> Contact us </p>
+        </div>
+        <Row container spacing={3} className={styles.footer__top__inner}>
+          <Col xs="12" sm="6" md="2" className={styles.footer__top__col}>
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              href="contact@REALMexico.com"
+              className={styles.icon}
+            />
+            <p>By mail</p>
+            <p className={styles.link}>
+              <Link to={`#`}>contact@REALMexico.com</Link>
+            </p>
+          </Col>
+          <Col xs="12" sm="6" md="2" className={styles.footer__top__col}>
+            <FontAwesomeIcon icon={faCalendarAlt} className={styles.icon} />
+            <p>Every day on weeks days</p>
+            <p>From 8:00 till 16:00</p>
+          </Col>
+          <Col xs="12" sm="6" md="2" className={styles.footer__top__col}>
+            <FontAwesomeIcon icon={faPhone} className={styles.icon} />
+            <p>By phone</p>
+            <p>+52-1-10766-655</p>
+          </Col>
+          <Col xs="12" sm="6" md="2" className={styles.footer__top__col}>
+            <FontAwesomeIcon icon={faMapMarkedAlt} className={styles.icon} />
+            <p>Our agency</p>
+            <p>Avenida Merida</p>
+          </Col>
+          <Col xs="12" sm="6" md="2" className={styles.footer__top__col}>
+            <FontAwesomeIcon icon={faMapMarkedAlt} className={styles.icon} />
+            <p>On social media</p>
+            <Link href="#">
+              <FontAwesomeIcon
+                icon={faFacebookF}
+                href="#"
+                className={styles.icon}
+              />
+            </Link>
+            <Link href="#">
+              <FontAwesomeIcon icon={faTwitter} className={styles.icon} />
+            </Link>
+            <Link href="#">
+              <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+            </Link>
+            <Link href="#">
+              <FontAwesomeIcon icon={faPinterest} className={styles.icon} />
+            </Link>
+            <Link href="#">
+              <FontAwesomeIcon icon={faGooglePlusG} className={styles.icon} />
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+      <Container className={styles.footer__neewsletter}>
+        <Row>
+          <Col xs="12" sm="12" md="6">
+            <p>Follow our team of experts, live from our destinations.</p>
+            <p>
+              Subscribe to our monthly newsletter to receive the latest news and
+              tips from Terra agencies all over the world.
+            </p>
+          </Col>
+          <Col xs="12" sm="12" md="4">
+            <div className={styles.iconBox}>
+              <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
+            </div>
+            <input
+              icon={faLongArrowAltRight}
+              type="text"
+              placeholder="Enter your email address"
+              required
+            />
+          </Col>
+          <Col xs="12" sm="12" md="2">
+            <Button>
+              <FontAwesomeIcon
+                className={styles.icon}
+                icon={faLongArrowAltRight}
+              ></FontAwesomeIcon>
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
     {children}
   </div>
 );
@@ -28,7 +133,7 @@ Component.propTypes = {
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+// const footer = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   Component as Footer,
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,19 +1,78 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Nav,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+import Badge from "@material-ui/core/Badge";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
-import clsx from 'clsx';
+import clsx from "clsx";
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
-import styles from './Header.module.scss';
+import styles from "./Header.module.scss";
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Header</h2>
-    {children}
-  </div>
-);
+const Component = ({ className, children }) => {
+  return (
+    <div className={clsx(className, styles.root)}>
+      <div className={styles.container}>
+        <div className={styles.container__logo}>
+          <Link to={"/"}>
+            <img src="/images/logo.png" alt="Logo" title="REALMexico" />
+          </Link>
+        </div>
+        <div className={styles.container__navbar}>
+          <Navbar light expand="md">
+            <Nav className="ml-auto" navbar>
+              <Link to={"/offers/1"} className={styles.navItem}>
+                Riviera Maya
+              </Link>
+              <Link to={"/offers/2"} className={styles.navItem}>
+                Oaxaca
+              </Link>
+              <Link to={"/offers/3"} className={styles.navItem}>
+                Chiapas
+              </Link>
+              <Link to={"/offers/4"} className={styles.navItem}>
+                Jalisco
+              </Link>
+              <Link to={"/offers/5"} className={styles.navItem}>
+                Ciudad de Mexico
+              </Link>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret className={styles.dropdown}>
+                  Our agency
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Team</DropdownItem>
+                  <DropdownItem>10 steps to book a trip with us</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Contact</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Navbar>
+        </div>
+        <div className={styles.container__cart}>
+          <Link to={"/cart"} className={styles.link}>
+            <Badge color="secondary" badgeContent={"0"}>
+              <FontAwesomeIcon icon={faShoppingCart} className={styles.icon} />
+            </Badge>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 Component.propTypes = {
   children: PropTypes.node,
